72e29a542bd3c346943f36594f1c3337673d3a4f
Gram <master_fess@mail.ru>
2020-09-25 12:25:39 +0200

    use gopath

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@ set -e
 mkdir -p build
 cp ./frontend/* ./build/
 cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" ./build/script.js
-statik -src=./include/ -dest=./wasm/
+
+export GOPATH=$(go env GOPATH)
+$GOPATH/bin/statik -src=./include/ -dest=./wasm/
+
 GOOS=js GOARCH=wasm go build -o build/frontend.wasm ./wasm/
